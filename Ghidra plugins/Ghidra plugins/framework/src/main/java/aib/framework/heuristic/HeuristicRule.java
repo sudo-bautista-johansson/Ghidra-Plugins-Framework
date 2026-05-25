2e19e3501e558f9de767a7831fc878dd1b24c948
sudo-bautista-johansson <johanssonbautista12@gmail.com>
2026-05-24 21:58:38 -0300

    Delete Ghidra plugins/Ghidra plugins directory

--- Ghidra plugins/Ghidra plugins/framework/src/main/java/aib/framework/heuristic/HeuristicRule.java
+++
@@ -1,9 +0,0 @@
-package aib.framework.heuristic;
-
-import aib.framework.model.FunctionContext;
-import aib.framework.model.HeuristicFinding;
-import java.util.List;
-
-public interface HeuristicRule {
-    List<HeuristicFinding> evaluate(FunctionContext functionContext);
-}
